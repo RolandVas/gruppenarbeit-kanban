ec211d950329a4366c1b8423bceafd73679c6358
RolandVas <rolandvas@hotmail.com>
2022-03-09 17:31:18 +0100

    Add Task fertig

--- assets/js/Roland-main.js
+++ assets/js/Roland-main.js
@@ -1,4 +1,4 @@
-
+/* Aktueller Zeit und Datum */
 let today = new Date();
 let time = ('0' + today.getHours()).slice(-2) + ':' + ('0' + today.getMinutes()).slice(-2);
 let date = today.getFullYear() + '.' + ('0' + (today.getMonth() + 1)).slice(-2) + '.' + ('0' + today.getDate()).slice(-2);
@@ -7,16 +7,25 @@ let date = today.getFullYear() + '.' + ('0' + (today.getMonth() + 1)).slice(-2) 
 let allTasks = [];
 
 
-
+/* Wenn man auf dem Button Hinzufügen drückt werden alle eingegebene Werte ausgelesen und im Array allTasks gespeichert */
 function addTask() {
-    let description = document.getElementById('description').value;
+    let title = document.getElementById('title').value;
     let category = document.getElementById('category').value;
+    let description = document.getElementById('description').value;
+    let due_date = document.getElementById('due-date').value;
+    let ungency = document.getElementById('ungency').value;
+    let assigned = document.getElementById('assigned').value;
 
     let task = {
-        'description': description,
+        'title': title,
         'category': category,
         'createdTime': time,
         'createdDate': date,
+        'description': description,
+        'due-date': due_date,
+        'ungency': ungency,
+        'assigned': assigned
+
     }
     allTasks.push(task)
 
@@ -29,13 +38,14 @@ function addTask() {
 }
 
 
-
+/* allTasks wird im localStorage gespeichert bwz. aktualisiert */
 function save() {
     let allTasksAsString = JSON.stringify(allTasks);
     localStorage.setItem('allTasks', allTasksAsString)
 }
 
 
+/* alle gespeicherte Werte aus dem localStorage werden abgerufen und ald Ticket dargestellt */
 function loadTasks() {
     let allTasksAsString = localStorage.getItem('allTasks');
     allTasks = JSON.parse(allTasksAsString);
@@ -45,23 +55,25 @@ function loadTasks() {
         const element = allTasks[i];
         document.getElementById('showTasks').innerHTML += `
     <div class="task">
-        <div>${element['description']}</div>
-        <div>${element['category']}</div>
-        <div>${element['createdTime']}</div>
-        <div>${element['createdDate']}</div>
+        <div><b>Titel:</b> ${element['title']}</div>
+        <div><b>Kategorie:</b> ${element['category']}</div>
+        <div><b>Erstellt am:</b> ${element['createdTime']} ${element['createdDate']}</div>
+        <div><b>Beschreibung:</b> ${element['description']}</div>
+        <div><b>Bis zu</b> ${element['due-date']} <b>erledigen</b></div>
+        <div><b>Dringlichkeit:</b> ${element['ungency']}</div>
+        <div><b>Zugeteilt für:</b> ${element['assigned']}</div>
         <div class="deletetask" onclick=deleteTask(${i})>Delete</div>
     </div>
     `;
     }
     
-
 }
 
+
+/* Ticket löschen und aktualisieren */
 function deleteTask(i) {
     allTasks.splice(i, 1);
-    // localStorage.removeItem([i]);
 
-    
     save();
     loadTasks();
 }
